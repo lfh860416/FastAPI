1369b0b5ca9391bc9676fec8ab06f8a45c08e5c8
fenhua.lin <fenhua.lin@XXHAIR.local>
2023-05-10 09:42:20 +0200

    update config.py

--- app/config.py
+++ app/config.py
@@ -1,16 +1,17 @@
 from pydantic import BaseSettings
+import os
 
 class Settings(BaseSettings):
-    database_hostname: str = "localhost"
-    database_port: str = "5432"
-    database_name: str = "fastapi"
-    database_username: str = "postgres"
-    database_password: str = "170213"
-    secret_key: str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-    algorithm: str = "HS256"
-    access_token_expire_minutes: int = 30
+    database_hostname: str
+    database_port: str
+    database_name: str
+    database_username: str
+    database_password: str
+    secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int
 
     class Config:
-        env_file = ".env"
+        env_file = 'app/.env'
 
-settings = Settings() 
+settings = Settings()
